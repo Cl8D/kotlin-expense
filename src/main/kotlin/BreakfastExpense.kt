214e6cf2d37edf33f7bb17743f3b25b9d2aa9156
jiwon <ljwon77@daum.net>
2024-02-25 13:08:39 +0900

    비용의 구체 클래스들의 함수 기능을 정리하기

--- src/main/kotlin/BreakfastExpense.kt
+++ src/main/kotlin/BreakfastExpense.kt
@@ -1,19 +1,5 @@
 class BreakfastExpense(amount: Int) : Expense(Type.BREAKFAST, amount) {
-    override fun getName(): String {
-        var name = "TILT"
-
-        when (this.type) {
-            Type.DINNER -> name = "Dinner"
-            Type.BREAKFAST -> name = "Breakfast"
-            Type.CAR_RENTAL -> name = "Car Rental"
-        }
-        return name
-    }
-
-    override fun isMeal() = this.type == Type.BREAKFAST || this.type == Type.DINNER
-    override fun isOverage() = (this.type == Type.DINNER
-            && this.amount > 5000
-            || this.type === Type.BREAKFAST
-            && this.amount > 1000)
-
+    override fun getName(): String = "Breakfast"
+    override fun isMeal() = true
+    override fun isOverage() = this.amount > 1000
 }
--- src/main/kotlin/CarRentalExpense.kt
+++ src/main/kotlin/CarRentalExpense.kt
@@ -1,19 +1,5 @@
 class CarRentalExpense(amount: Int) : Expense(Type.CAR_RENTAL, amount) {
-    override fun getName(): String {
-        var name = "TILT"
-
-        when (this.type) {
-            Type.DINNER -> name = "Dinner"
-            Type.BREAKFAST -> name = "Breakfast"
-            Type.CAR_RENTAL -> name = "Car Rental"
-        }
-        return name
-    }
-
-    override fun isMeal() = this.type == Type.BREAKFAST || this.type == Type.DINNER
-    override fun isOverage() = (this.type == Type.DINNER
-            && this.amount > 5000
-            || this.type === Type.BREAKFAST
-            && this.amount > 1000)
-
+    override fun getName(): String = "Car Rental"
+    override fun isMeal() = false
+    override fun isOverage() = false
 }
